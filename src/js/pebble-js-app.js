227a782ad2b68fdc824515a1501ec78e51e735a6
Samuel Rinnetmäki <samuel@iki.fi>
2015-04-11 18:43:25 +0300

    make sure numbers are numbers

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -4,9 +4,9 @@ var messageQueue = [];
 Pebble.addEventListener("ready",
   function(e) {
     config = JSON.parse(localStorage.getItem("config") || "{}");
-    config["work"] = config["work"] || 90;
-    config["rest"] = config["rest"] || 30;
-    config["repeat"] = config["repeat"] || 4;
+    config["work"] = parseInt(config["work"]) || 90;
+    config["rest"] = parseInt(config["rest"]) || 30;
+    config["repeat"] = parseInt(config["repeat"]) || 4;
     config["routines"] = config["routines"] || ["Push-ups", "Sit-ups", "Lunges", "Pull-ups"];
     console.log("JavaScript app ready and running!");
     initialized = true;
@@ -35,9 +35,9 @@ Pebble.addEventListener("webviewclosed",
 function sendConfig(config) {
   var msg = {};
   config["routines"] = config["routines"] || []; // just in case
-  msg["0"] = config["work"] || 90;
-  msg["1"] = config["rest"] || 30;
-  msg["2"] = config["repeat"] || 4;
+  msg["0"] = parseInt(config["work"]) || 90;
+  msg["1"] = parseInt(config["rest"]) || 30;
+  msg["2"] = parseInt(config["repeat"]) || 4;
   msg["3"] = config["routines"].length;
   for (var i=0; i<config["routines"].length; i++) {
     msg[i+4] = config["routines"][i];
